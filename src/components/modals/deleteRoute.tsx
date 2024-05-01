4f1888c088d4d84e276a508dfec21a2d1854f3a4
LovelessCodes <hello@loveless.codes>
2024-05-01 14:31:16 +0100

    changed: ordered imports on deleteRoute modal

--- src/components/modals/deleteRoute.tsx
+++ src/components/modals/deleteRoute.tsx
@@ -1,6 +1,5 @@
-import { useSession } from "next-auth/react";
-import { useState } from "react";
-import { api } from "~/utils/api";
+import { useSession } from 'next-auth/react';
+import { api } from '~/utils/api';
 
 const DeleteRoute = ({route}: {route: string}) => {
     const { data: sessionData } = useSession();
